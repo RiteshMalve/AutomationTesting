4aa3115a23806e4ddf08a927122664bf991743fe
RiteshMalve <riteshmalve1@gmail.com>
2022-10-04 16:36:19 +0530

    Handling alert code aadded

---
+++ src/test/AlertHandling.java
@@ -0,0 +1,24 @@
+package test;
+
+import java.time.Duration;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AlertHandling {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver","D:\\driver\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.navigate().to("https://demoqa.com/alerts");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.findElement(By.id("alertButton")).click();
+        Alert myAlert = driver.switchTo().alert();
+        System.out.println(myAlert.getText());
+        myAlert.accept();
+	}
+
+}
