cebe8cbe8b3fea4c4046340be9799ebdee756378
RiteshMalve <riteshmalve1@gmail.com>
2022-10-04 12:01:10 +0530

    first release of hotelApp

---
+++ src/test/MyFirstAutomationScript.java
@@ -0,0 +1,22 @@
+package test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MyFirstAutomationScript {
+
+	public static void main(String[] args) {
+
+	System.setProperty("webdriver.chrome.driver","D:\\driver\\chromedriver_win32\\chromedriver.exe");
+	WebDriver driver = new ChromeDriver();
+	driver.get("https://adactinhotelapp.com");
+	driver.findElement(By.id("username")).sendKeys("riteshmalve1");
+	driver.findElement(By.id("password")).sendKeys("rr25111971");
+	driver.findElement(By.id("login")).click();
+	
+	
+
+	}
+
+}
