4bebe2fca66e8ed5ece69a4e1244c9932b19b5f7
RiteshMalve <63526358+RiteshMalve@users.noreply.github.com>
2022-10-06 11:17:21 +0530

    Update AlertHandling.java

--- src/test/AlertHandling.java
+++ src/test/AlertHandling.java
@@ -18,6 +18,7 @@ public class AlertHandling {
 		driver.findElement(By.id("alertButton")).click();
         Alert myAlert = driver.switchTo().alert();
         System.out.println(myAlert.getText());
+	System.out.println("I am in alert handling code");
         myAlert.accept();
 	}
 
